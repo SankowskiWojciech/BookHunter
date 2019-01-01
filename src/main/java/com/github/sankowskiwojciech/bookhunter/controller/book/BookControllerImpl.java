042b61a913a6d603bd858b5532e32dd51e9156a2
wojciech <sankowski.wojciech@gmail.com>
2019-01-02 00:55:10 +0100

    partially added book view

--- src/main/java/com/github/sankowskiwojciech/bookhunter/controller/book/BookControllerImpl.java
+++ src/main/java/com/github/sankowskiwojciech/bookhunter/controller/book/BookControllerImpl.java
@@ -6,21 +6,34 @@ import com.github.sankowskiwojciech.bookhunter.model.book.BookResponse;
 import com.github.sankowskiwojciech.bookhunter.service.book.BookService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
-@RestController
-@RequestMapping(value = "api/book")
+@Controller
+@RequestMapping(value = "/book")
 @AllArgsConstructor(onConstructor = @__(@Autowired))
 public class BookControllerImpl {
 
     private final BookService bookService;
 
     @GetMapping("/{bookId}")
-    public BookResponse findBookById(@PathVariable String bookId) {
+    public ModelAndView findBookById(@PathVariable String bookId) {
         Book book = bookService.findBookByBookId(bookId);
-        return new BookToBookResponse().apply(book);
+
+        ModelAndView modelAndView = new ModelAndView("book/book");
+
+        BookResponse bookResponse = new BookToBookResponse().apply(book);
+
+        modelAndView.addObject("title", bookResponse.getTitle());
+        modelAndView.addObject("releaseYear", bookResponse.getReleaseYear());
+        modelAndView.addObject("description", bookResponse.getDescription());
+        modelAndView.addObject("country", bookResponse.getCountry());
+        modelAndView.addObject("categories", bookResponse.getCategories());
+        modelAndView.addObject("authors", bookResponse.getAuthors());
+
+        return modelAndView;
     }
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,8 +3,18 @@ INSERT INTO USER_AUTHENTICATION (USER_NAME, PASSWORD, EMAIL_ADDRESS) VALUES ('te
 INSERT INTO USER_AUTHENTICATION (USER_NAME, PASSWORD, EMAIL_ADDRESS) VALUES ('test3', '$2a$11$jzr7uyY0LVdqgGehTcsZQ.xoBot/J3Kxlgdn7QkLoCx48L16O86aK', 'test3@gmail.com');
 
 INSERT INTO AUTHOR (ID, NAME, SURNAME, BIRTH_YEAR, DEATH_YEAR, PROFILE_PHOTO, DESCRIPTION, CATEGORIES) VALUES (1, 'test name', 'test surname', '1996', '2106', NULL, 'test description', 'SCIENCE_FICTION BIOGRAPHY');
+INSERT INTO AUTHOR (ID, NAME, SURNAME, BIRTH_YEAR, DEATH_YEAR, PROFILE_PHOTO, DESCRIPTION, CATEGORIES) VALUES (2, 'Wojciech', 'Sankowski', '1996', NULL, NULL, 'Junior Java Developer', 'SCIENCE ART');
+INSERT INTO AUTHOR (ID, NAME, SURNAME, BIRTH_YEAR, DEATH_YEAR, PROFILE_PHOTO, DESCRIPTION, CATEGORIES) VALUES (3, 'Matylda', 'Wacławska', '1995', NULL, NULL, 'Great chemist!', 'ART CLASSIC');
 
-INSERT INTO BOOK (ISBN, TITLE, ORIGINAL_TITLE, RELEASE_YEAR, CATEGORIES, DESCRIPTION, COUNTRY_CODE, COVER_IMAGE) VALUES ('1234567890123', 'Test Title', 'Test Original Title', '1996', 'SCIENCE_FICTION BIOGRAPHY', 'Test Description', 'PL', NULL);
+INSERT INTO BOOK (ISBN, TITLE, ORIGINAL_TITLE, RELEASE_YEAR, CATEGORIES, DESCRIPTION, COUNTRY_CODE, COVER_IMAGE) VALUES ('1234567890123', 'Brief Answers to the Big Questions', 'Brief Answers to the Big Questions', '2018', 'SCIENCE BIOGRAPHY HUMOR', 'Stephen Hawking was the most renowned scientist since Einstein, known both for his groundbreaking work in physics and cosmology and for his mischievous sense of humor. He educated millions of readers about the origins of the universe and the nature of black holes, and inspired millions more by defying a terrifying early prognosis of ALS, which originally gave him only two years to live. In later life he could communicate only by using a few facial muscles, but he continued to advance his field and serve as a revered voice on social and humanitarian issues.
+
+Hawking not only unraveled some of the universe’s greatest mysteries but also believed science plays a critical role in fixing problems here on Earth. Now, as we face immense challenges on our planet—including climate change, the threat of nuclear war, and the development of artificial intelligence—he turns his attention to the most urgent issues facing us.
+
+Will humanity survive? Should we colonize space? Does God exist? These are just a few of the questions Hawking addresses in this wide-ranging, passionately argued final book from one of the greatest minds in history.
+
+Featuring a foreword by Eddie Redmayne, who won an Oscar for playing Stephen Hawking, an introduction by Nobel Laureate Kip Thorne, and an afterword from Hawking’s daughter, Lucy, Brief Answers to the Big Questions is a brilliant last message to the world. ', 'US', NULL);
 INSERT INTO BOOK (ISBN, TITLE, ORIGINAL_TITLE, RELEASE_YEAR, CATEGORIES, DESCRIPTION, COUNTRY_CODE, COVER_IMAGE) VALUES ('0987654321098', 'Test Title 2', 'Test Original Title 2', '2009', 'ROMANCE BIOGRAPHY RELIGION', 'Test Description 2', 'RU', NULL);
 
 INSERT INTO BOOK_AUTHOR_RELATIONS (ID, BOOK_ID, AUTHOR_ID) VALUES (1, '1234567890123', '1');
+INSERT INTO BOOK_AUTHOR_RELATIONS (ID, BOOK_ID, AUTHOR_ID) VALUES (2, '1234567890123', '2');
+INSERT INTO BOOK_AUTHOR_RELATIONS (ID, BOOK_ID, AUTHOR_ID) VALUES (3, '1234567890123', '3');
